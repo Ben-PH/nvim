9f2db55b706a23d541de2f97c1e5d30d0bbc5ebc
Ben PHL <benphawke@gmail.com>
2023-03-19 20:30:58 +0100

    which-key for goto-definition

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,4 +1,5 @@
 local lsp = require('lsp-zero')
+local wk = require('which-key')
 
 lsp.preset('recommended')
 lsp.ensure_installed({
@@ -9,7 +10,11 @@ lsp.ensure_installed({
 lsp.on_attach(function (client, bufnr)
 	local opts = {buffer = bufnr, remap = false}
 
-	vim.keymap.set("n", ",gd", function() vim.lsp.buf.definition() end, opts) 
-	
+	wk.register({
+		g = { 
+			name = "goto", 
+			d = { function() vim.lsp.buf.definition() end, "definition", buffer = bufnr, noremap = true },
+		},
+	},{prefix = ","})
 end)
 lsp.setup()
