1f9a6710d226fdf3c112686c4100b5ea29136d93
Ben PHL <benphawke@gmail.com>
2023-04-15 18:28:23 +0200

    More binidngs moved to bindings.lua

--- after/plugin/bindings.lua
+++ after/plugin/bindings.lua
@@ -1,3 +1,5 @@
+local mark = require("harpoon.mark")
+local ui = require("harpoon.ui")
 
 local builtin = require('telescope.builtin')
 local wk = require("which-key")
@@ -13,4 +15,31 @@ wk.register({
 		b = { builtin.buffers, "buffer search" },
 		h = { builtin.help_tags, "help search" },
 	},
+	h = { 
+		name = "Harpoon", 
+		a = { mark.add_file, "Add mark" },
+		e = {ui.toggle_quick_menu, "Show quick menu"}
+	},
 },{prefix = "<leader>"})
+
+local mark = require("harpoon.mark")
+local ui = require("harpoon.ui")
+
+vim.keymap.set("n", "<C-h>", function() ui.nav_file(1) end)
+vim.keymap.set("n", "<C-t>", function() ui.nav_file(2) end)
+vim.keymap.set("n", "<C-n>", function() ui.nav_file(3) end)
+vim.keymap.set("n", "<C-s>", function() ui.nav_file(4) end)
+
+
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("x", "<leader>p", "\"_dP")
+
+-- Moeve things up and down, respecting indent
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+
+vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
+vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
