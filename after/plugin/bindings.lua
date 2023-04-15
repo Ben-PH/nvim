ced9a9f604acecb901df7aa104484afd40348505
Ben PHL <benphawke@gmail.com>
2023-04-15 22:14:06 +0200

    Clean up bindings, add some of primes awesome bindings

--- after/plugin/bindings.lua
+++ after/plugin/bindings.lua
@@ -4,12 +4,12 @@ local builtin = require('telescope.builtin')
 local wk = require("which-key")
 
 wk.register({
-	a = { 
-		name = "application", 
+	a = {
+		name = "application",
 		u = { vim.cmd.UndotreeToggle, "Undo Tree" },
 	},
-	s = { 
-		name = "search", 
+	s = {
+		name = "search",
 		f = { builtin.find_files, "file search" },
 		g = { builtin.live_grep, "live-grep" },
 		b = { builtin.buffers, "buffer search" },
@@ -18,14 +18,14 @@ wk.register({
 	},
 	g = {
 		name = "git",
-		s = {"<cmd>Git<cr>", "Git"},
+		s = { "<cmd>Git<cr>", "Git" },
 	},
-	h = { 
-		name = "Harpoon", 
+	h = {
+		name = "Harpoon",
 		a = { mark.add_file, "Add mark" },
-		e = {ui.toggle_quick_menu, "Show quick menu"}
+		e = { ui.toggle_quick_menu, "Show quick menu" }
 	},
-},{prefix = "<leader>"})
+}, { prefix = "<leader>" })
 
 vim.keymap.set("n", "<C-h>", function() ui.nav_file(1) end)
 vim.keymap.set("n", "<C-t>", function() ui.nav_file(2) end)
@@ -42,5 +42,22 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
 vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+vim.keymap.set("n", "<leader>y", "\"+y")
+vim.keymap.set("v", "<leader>y", "\"+y")
+vim.keymap.set("n", "<leader>Y", "\"+Y")
+
+
+vim.keymap.set("n", "Q", "<nop>")
+
+vim.keymap.set("n", "<leader>f", function() vim.lsp.buf.format() end)
