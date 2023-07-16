6cfa0127cdced09d4c19d43015483432fc232d70
Ben PHL <benphawke@gmail.com>
2023-07-16 17:13:26 +0200

    Revine whichkey settings

--- lua/core/packer.lua
+++ lua/core/packer.lua
@@ -11,12 +11,12 @@ use {
       plugins = {
         marks = true, -- shows a list of your marks on ' and `
         registers = true, -- shows your registers on " in NORMAL or <C-r> in INSERT mode
+        spelling = {
+          enabled = false, -- enabling this will show WhichKey when pressing z= to select spelling suggestions
+        },
+
         -- the presets plugin, adds help for a bunch of default keybindings in Neovim
         -- No actual key bindings are created
-        spelling = {
-          enabled = true, -- enabling this will show WhichKey when pressing z= to select spelling suggestions
-          suggestions = 20, -- how many suggestions should be shown in the list?
-        },
         presets = {
           operators = true, -- adds help for operators like d, y, ...
           motions = true, -- adds help for motions
@@ -33,9 +33,9 @@ use {
       key_labels = {
         -- override the label used to display some keys. It doesn't effect WK in any other way.
         -- For example:
-        -- ["<space>"] = "SPC",
-        -- ["<cr>"] = "RET",
-        -- ["<tab>"] = "TAB",
+        ["<space>"] = "SPC",
+        ["<cr>"] = "RET",
+        ["<tab>"] = "TAB",
       },
       motions = {
         count = true,
@@ -52,8 +52,8 @@ use {
       window = {
         border = "none", -- none, single, double, shadow
         position = "bottom", -- bottom, top
-        margin = { 1, 0, 1, 0 }, -- extra window margin [top, right, bottom, left]. When between 0 and 1, will be treated as a percentage of the screen size.
-        padding = { 1, 2, 1, 2 }, -- extra window padding [top, right, bottom, left]
+        margin = { 0, 0, 0, 0 }, -- extra window margin [top, right, bottom, left]. When between 0 and 1, will be treated as a percentage of the screen size.
+        padding = { 0,0,0,0 }, -- extra window padding [top, right, bottom, left]
         winblend = 0, -- value between 0-100 0 for fully opaque and 100 for fully transparent
         zindex = 1000, -- positive value to position WhichKey above other floating windows.
       },
