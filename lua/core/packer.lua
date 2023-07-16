89cc198a2655c225f489eed4831d65f6882fa18b
Ben <benphawke@gmail.com>
2023-07-16 22:36:20 +0200

    plugin/whichkey (#1)
    * Add packer with default settings
    
    * Revine whichkey settings
    
    * Move config to dedicated file

--- lua/core/packer.lua
+++ lua/core/packer.lua
@@ -1,3 +1,11 @@
 return require('packer').startup(function(use)
   use {'wbthomason/packer.nvim'}
+
+
+  use {
+    "folke/which-key.nvim",
+    config = function()
+      require('core.plugin_configs.which_key')
+    end
+  }
 end)
