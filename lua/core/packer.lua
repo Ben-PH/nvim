fb54a197f484f85f244bd8d6fa12f22e3fdc6a6f
Ben PHL <benphawke@gmail.com>
2023-07-17 17:47:44 +0200

    add telescope file browser

--- lua/core/packer.lua
+++ lua/core/packer.lua
@@ -13,6 +13,10 @@ return require('packer').startup(function(use)
     'nvim-telescope/telescope.nvim', tag = '0.1.2',
     requires = { {'nvim-lua/plenary.nvim'} },
   }
+  use {
+      "nvim-telescope/telescope-file-browser.nvim",
+      requires = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" }
+  }
 
   use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
   use ( 'theprimeagen/harpoon' )
