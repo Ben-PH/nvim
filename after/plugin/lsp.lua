c121e91ef7e867afaefe43b22a022e67b525df5c
Ben PHL <benphawke@gmail.com>
2023-07-18 13:53:55 +0200

    Setup styleing

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -4,32 +4,32 @@ local lspconf = require("lspconfig")
 lsp.preset("recommended")
 
 lsp.ensure_installed({
-  'rust_analyzer',
+	"rust_analyzer",
 })
 
 -- Fix Undefined global 'vim'
 lsp.nvim_workspace()
 
 lsp.set_preferences({
-    suggest_lsp_servers = false,
-    sign_icons = {
-        error = 'E',
-        warn = 'W',
-        hint = 'H',
-        info = 'I'
-    }
+	suggest_lsp_servers = false,
+	sign_icons = {
+		error = "E",
+		warn = "W",
+		hint = "H",
+		info = "I",
+	},
 })
 
 local navbuddy = require("nvim-navbuddy")
 lsp.on_attach(function(client, bufnr)
-  local opts = {buffer = bufnr, remap = false}
-  -- remaps here
-  navbuddy.attach(client, bufnr)
-  vim.keymap.set("n", ",gd", "<cmd>lua vim.lsp.buf.definition()<CR>")
+	local opts = { buffer = bufnr, remap = false }
+	-- remaps here
+	navbuddy.attach(client, bufnr)
+	vim.keymap.set("n", ",gd", "<cmd>lua vim.lsp.buf.definition()<CR>")
 end)
 
 lsp.setup()
 
 vim.diagnostic.config({
-    virtual_text = true
+	virtual_text = true,
 })
--- lua/core/packer.lua
+++ lua/core/packer.lua
@@ -1,81 +1,81 @@
-vim.cmd [[packadd packer.nvim]]
-return require('packer').startup(function(use)
-  use {'wbthomason/packer.nvim'}
+vim.cmd([[packadd packer.nvim]])
+return require("packer").startup(function(use)
+	use({ "wbthomason/packer.nvim" })
 
+	use({
+		"folke/which-key.nvim",
+		config = function()
+			require("core.plugin_configs.which_key")
+		end,
+	})
+	use({
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.2",
+		requires = { { "nvim-lua/plenary.nvim" } },
+	})
+	use({
+		"nvim-telescope/telescope-file-browser.nvim",
+		requires = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
+	})
+	use("ahmedkhalf/project.nvim")
 
-  use {
-    "folke/which-key.nvim",
-    config = function()
-      require('core.plugin_configs.which_key')
-    end
-  }
-  use {
-    'nvim-telescope/telescope.nvim', tag = '0.1.2',
-    requires = { {'nvim-lua/plenary.nvim'} },
-  }
-  use {
-      "nvim-telescope/telescope-file-browser.nvim",
-      requires = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" }
-  }
-  use ("ahmedkhalf/project.nvim")
+	use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" })
+	use("theprimeagen/harpoon")
 
-  use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
-  use ( 'theprimeagen/harpoon' )
+	use({
+		"VonHeikemen/lsp-zero.nvim",
+		branch = "v1.x",
+		requires = {
+			-- LSP Support
+			{ "neovim/nvim-lspconfig" },
+			{ "williamboman/mason.nvim" },
+			{ "williamboman/mason-lspconfig.nvim" },
 
-  use {
-	  'VonHeikemen/lsp-zero.nvim',
-	  branch = 'v1.x',
-	  requires = {
-		  -- LSP Support
-		  {'neovim/nvim-lspconfig'},
-		  {'williamboman/mason.nvim'},
-		  {'williamboman/mason-lspconfig.nvim'},
+			-- Autocompletion
+			{ "hrsh7th/nvim-cmp" },
+			{ "hrsh7th/cmp-buffer" },
+			{ "hrsh7th/cmp-path" },
+			{ "saadparwaiz1/cmp_luasnip" },
+			{ "hrsh7th/cmp-nvim-lsp" },
+			{ "hrsh7th/cmp-nvim-lua" },
 
-		  -- Autocompletion
-		  {'hrsh7th/nvim-cmp'},
-		  {'hrsh7th/cmp-buffer'},
-		  {'hrsh7th/cmp-path'},
-		  {'saadparwaiz1/cmp_luasnip'},
-		  {'hrsh7th/cmp-nvim-lsp'},
-		  {'hrsh7th/cmp-nvim-lua'},
+			-- Snippets
+			{ "L3MON4D3/LuaSnip" },
+			{ "rafamadriz/friendly-snippets" },
+		},
+	})
 
-		  -- Snippets
-		  {'L3MON4D3/LuaSnip'},
-		  {'rafamadriz/friendly-snippets'},
-	  }
-  }
+	use("folke/zen-mode.nvim")
+	use("folke/trouble.nvim")
+	use({ "tanvirtin/vgit.nvim", requires = { "nvim-lua/plenary.nvim" } })
+	use({
+		"folke/noice.nvim",
+		requires = {
+			-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+			{ "MunifTanjim/nui.nvim" },
+			-- OPTIONAL:
+			--   `nvim-notify` is only needed, if you want to use the notification view.
+			--   If not available, we use `mini` as the fallback
+			{ "rcarriga/nvim-notify" },
+		},
+	})
 
-  use ("folke/zen-mode.nvim")
-  use ("folke/trouble.nvim")
-  use { 'tanvirtin/vgit.nvim', requires = { 'nvim-lua/plenary.nvim' } }
-  use {
-    "folke/noice.nvim",
-    requires = {
-      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-      {"MunifTanjim/nui.nvim"},
-      -- OPTIONAL:
-      --   `nvim-notify` is only needed, if you want to use the notification view.
-      --   If not available, we use `mini` as the fallback
-      {"rcarriga/nvim-notify"},
-    }
-  }
+	use({
+		"kylechui/nvim-surround",
+		tag = "*", -- Use for stability; omit to use `main` branch for the latest features
+	})
 
-  use({
-    "kylechui/nvim-surround",
-    tag = "*", -- Use for stability; omit to use `main` branch for the latest features
-  })
+	use({
+		"SmiteshP/nvim-navbuddy",
+		requires = {
+			"neovim/nvim-lspconfig",
+			"SmiteshP/nvim-navic",
+			"MunifTanjim/nui.nvim",
+			"numToStr/Comment.nvim", -- Optional
+			"nvim-telescope/telescope.nvim", -- Optional
+		},
+	})
 
-  use {
-      "SmiteshP/nvim-navbuddy",
-      requires = {
-          "neovim/nvim-lspconfig",
-          "SmiteshP/nvim-navic",
-          "MunifTanjim/nui.nvim",
-          "numToStr/Comment.nvim",        -- Optional
-          "nvim-telescope/telescope.nvim" -- Optional
-      }
-  }
-
-  use ("ggandor/leap.nvim")
-  use ("ThePrimeagen/harpoon")
+	use("ggandor/leap.nvim")
+	use("ThePrimeagen/harpoon")
 end)
