9f911e227538107887709eb90ca9bfb8f87dd9c5
Ben <benphawke@gmail.com>
2023-07-18 13:48:16 +0200

    plugin/initial_setup (#2)
    * Add telescope with color picker
    
    * add treesitter
    
    * Add harpoon plugin
    
    * Add lsp-zero
    
    * activate lsp-zero
    
    * add zen-mode and trouble
    
    * charge colors file to telescope-preview
    
    * add git-gutter
    
    * add noice
    
    * add nvim-surround
    
    * add nav-buddy
    
    * fix noice notifications
    
    * add leap
    
    * fix nvim-surround
    
    * add telescope file browser
    
    * add project(ile)
    
    * add harpoon
    
    * add goto definition

---
+++ after/plugin/lsp.lua
@@ -0,0 +1,35 @@
+local lsp = require("lsp-zero")
+local lspconf = require("lspconfig")
+
+lsp.preset("recommended")
+
+lsp.ensure_installed({
+  'rust_analyzer',
+})
+
+-- Fix Undefined global 'vim'
+lsp.nvim_workspace()
+
+lsp.set_preferences({
+    suggest_lsp_servers = false,
+    sign_icons = {
+        error = 'E',
+        warn = 'W',
+        hint = 'H',
+        info = 'I'
+    }
+})
+
+local navbuddy = require("nvim-navbuddy")
+lsp.on_attach(function(client, bufnr)
+  local opts = {buffer = bufnr, remap = false}
+  -- remaps here
+  navbuddy.attach(client, bufnr)
+  vim.keymap.set("n", ",gd", "<cmd>lua vim.lsp.buf.definition()<CR>")
+end)
+
+lsp.setup()
+
+vim.diagnostic.config({
+    virtual_text = true
+})
