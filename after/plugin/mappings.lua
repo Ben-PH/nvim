c3783e5790bbf41c1846c259cc5b8eb010230cf1
Ben PHL <benphawke@gmail.com>
2023-07-18 20:12:01 +0200

    add meta-bindings

---
+++ after/plugin/mappings.lua
@@ -0,0 +1,13 @@
+local wk = require("which-key")
+
+wk.register({
+
+    ["<M-b>"] = { "<cmd>Buffers<CR>", "Buffers" },
+    ["<M-g>"] = { "<cmd>Rg!<CR>", "grep" },
+    ["<M-f>"] = { "<cmd>Files<CR>", "find files" },
+    ["<M-l>"] = { "<cmd>Telescope projects<CR>", "list projects" },
+    ["<M-c>"] = { "<cmd>bp<CR>", "previous buffer" },
+    ["<M-r>"] = { "<cmd>bn<CR>", "next buffer" },
+    ["<M-/>"] = { "<cmd>nohlsearch<CR>", "Clear highlights" },
+    --["<M->"] = {"<cmd><CR>", ""},
+})
--- lua/core/packer.lua
+++ lua/core/packer.lua
@@ -1,6 +1,10 @@
 vim.cmd([[packadd packer.nvim]])
 return require("packer").startup(function(use)
     use({ "wbthomason/packer.nvim" })
+    use({
+        "junegunn/fzf.vim",
+        requires = { "junegunn/fzf", run = ":call fzf#install()" },
+    })
 
     use({
         "folke/which-key.nvim",
